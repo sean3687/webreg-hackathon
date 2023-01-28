ba01b10b5046a14c0df45b2ff4f7fa89a5430851
Sanath Nair <sanathnair09@gmail.com>
2023-01-28 11:49:14 -0800

    added basic code for header and banner

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="flex flex-col">
+      <Header />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import App from "./App";
 import { store } from "./app/store";
 import "./index.css";
 
@@ -10,7 +11,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <h1>hi</h1>
+      <App />
     </Provider>
   </React.StrictMode>
 );
